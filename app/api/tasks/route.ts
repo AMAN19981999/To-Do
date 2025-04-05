c017bb6da7af2b8b65da47d305e38defa428b897
AMAN19981999 <amanbhar0050@gmail.com>
2025-04-06 00:11:08 +0530

    assasasasasas

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,15 +1,15 @@
-// import { db } from '@/db/db';
-// import { tasks } from '@/db/schema';
-// import { NextResponse } from 'next/server';
+import { db } from '@/db/db';
+import { tasks } from '@/db/schema';
+import { NextResponse } from 'next/server';
 
-// export async function GET() {
-//   const allTasks = await db.select().from(tasks);
-//   return NextResponse.json(allTasks);
-// }
+export async function GET() {
+  const allTasks = await db.select().from(tasks);
+  return NextResponse.json(allTasks);
+}
 
-// export async function POST(req: Request) {
-//   const body = await req.json();
-//   const { title } = body;
-//   const newTask = await db.insert(tasks).values({ title }).returning();
-//   return NextResponse.json(newTask);
-// }
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { title } = body;
+  const newTask = await db.insert(tasks).values({ title }).returning();
+  return NextResponse.json(newTask);
+}

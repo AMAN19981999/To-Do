affa86ac54b2c1620369de2a4e5a601d4520c7f6
AMAN19981999 <amanbhar0050@gmail.com>
2025-04-06 22:30:16 +0530

    done the commit for route

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -8,14 +8,33 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function PATCH(req: NextRequest, context: { params: { id: string } }) {
-    const taskId = Number(context.params.id);
+// export async function PATCH(req: NextRequest, context: { params: { id: string } }) {
+//     const taskId = Number(context.params?.id);
+  
+//     if (isNaN(taskId)) {
+//       return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+//     }
+  
+//     const body = await req.json();
+//     const updated = await db
+//       .update(tasks)
+//       .set({ ...body })
+//       .where(eq(tasks.id, taskId))
+//       .returning();
+  
+//     return NextResponse.json(updated[0]);
+//   }
+
+
+export async function PATCH(req: NextRequest, context: any) {
+    const taskId = Number(context.params?.id);
   
     if (isNaN(taskId)) {
       return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
     }
   
     const body = await req.json();
+  
     const updated = await db
       .update(tasks)
       .set({ ...body })
@@ -24,6 +43,10 @@ export async function PATCH(req: NextRequest, context: { params: { id: string } 
   
     return NextResponse.json(updated[0]);
   }
+  
+
+
+
 
 export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }) {
     const { id } = await context.params; 
